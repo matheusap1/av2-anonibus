4223c5d621649cfa1b02ecd2fc4615ce359b86d8
matheusap1 <62668698+matheusap1@users.noreply.github.com>
2020-05-09 22:41:29 -0300

    Commit 22:40 - Dia 09/05/2020

--- App.js
+++ App.js
@@ -1,188 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import {
-  StyleSheet, View, Text, YellowBox, Image, ScrollView,
-  TextInput, TouchableOpacity
-} from 'react-native';
-import firebase from './config/firebase';
-import api from './services/axios';
-import axios from 'axios';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function Chat() {
+import Chat from './chat'
+import Home from './home'
 
-  const [user, setUser] = useState(null)
-  const [mensagens, setMensagens] = useState([])
-  const [caixaTexto, setCaixaTexto] = useState('')
-  const [scrollview, setScrollview] = useState('')
-  const [teste, setTeste] = useState('')
+export default function App() {
 
-
-  const db = firebase.firestore()
-  let mensagens_enviadas = []
-  const salvar = () => {
-    api.post('/enviarMensagem', {
-      mensagem: caixaTexto,
-      usuario: user.name,
-      avatar: user.picture,
-    })
-      .then(function () {
-        // setMensagens([...mensagens, caixaTexto])
-        setCaixaTexto('')
-        scrollview.scrollToEnd({ animated: true })
-      }).catch(function () {
-
-      })
-  }
-
-  useEffect(() => {
-    carregaUsuarioAnonimo()
-    let mensagens_enviadas = []
-    const unsubscribe = db.collection("chats")
-      .doc("sala_01").collection('mensagens')
-      .onSnapshot({ includeMetadataChanges: false }, function (snapshot) {
-        snapshot.docChanges().forEach(function (change) {
-          if (change.type === "added") {
-            const { mensagem, usuario, avatar } = change.doc.data()
-            const id = change.doc.id
-            mensagens_enviadas.push({ mensagem, usuario, avatar, id })
-          }
-        })
-        setMensagens([...mensagens_enviadas])
-        scrollview ? scrollview.scrollToEnd({ animated: true }) : null;
-      })
-    return () => {
-      unsubscribe()
-    }
-  }, [])
-
-  const carregaUsuarioAnonimo = () => {
-    axios.get('https://randomuser.me/api/')
-      .then(function (response) {
-        const user = response.data.results[0];
-        // setDistance(response.data.distance)
-        setUser({
-          name: `${user.name.first} ${user.name.last}`,
-          picture: user.picture.large
-        })
-        console.log('user', user)
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+  const Tab = createBottomTabNavigator();
 
   return (
-    <View style={styles.view}>
+    <NavigationContainer>
 
-      {user &&
-        <>
-          <TouchableOpacity onPress={carregaUsuarioAnonimo}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-            <Image
-              style={styles.avatar}
-              source={{ uri: user.picture }} />
-          </TouchableOpacity>
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Chat') {
+              iconName = focused ? 'ios-person' : 'ios-person';
+            }
 
-          <Text style={styles.nome_usuario}>{user.name}</Text>
-        </>
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Chat" component={Chat} />
 
-      }
+      </Tab.Navigator>
 
-
-
-      <ScrollView style={styles.scrollview} ref={(view) => { setScrollview(view) }}>
-        {
-          mensagens.length > 0 && mensagens.map(item => (
-
-            <View style={styles.linha_conversa}>
-              <Image style={styles.avatar_conversa} source={{ uri: item.avatar }} />
-              <View style={{ flexDirection: 'column', marginTop: 5 }}>
-                <Text style={{ fontSize: 12, color: '#999' }}>{item.usuario}</Text>
-                <Text>{item.mensagem}</Text>
-              </View>
-
-            </View>
-
-
-
-
-          ))
-        }
-      </ScrollView>
-
-
-      <View style={styles.footer}>
-        <TextInput
-          style={styles.input_mensagem}
-          onChangeText={text => setCaixaTexto(text)}
-          value={caixaTexto} />
-
-        <TouchableOpacity onPress={salvar}>
-          <Ionicons style={{ margin: 3 }} name="md-send" size={32} color={'#999'} />
-        </TouchableOpacity>
-      </View>
-
-
-
-    </View>)
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    alignItems: 'center',
-    alignContent: 'center',
-    width: '100%',
-    paddingTop: 50,
-    borderBottomWidth: 1,
-    borderColor: '#000'
-  },
-  avatar: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    borderWidth: 3,
-    borderColor: '#333'
-  },
-
-  avatar_conversa: {
-    width: 40,
-    height: 40,
-    borderRadius: 50,
-    borderWidth: 1,
-    borderColor: '#333',
-    marginRight: 10
-  },
-
-  nome_usuario: {
-    fontSize: 25,
-    color: '#999'
-  },
-
-  footer: {
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    height: 50
-  },
-  input_mensagem: {
-    borderColor: '#e6e6e6',
-    borderWidth: 1,
-    flex: 1,
-    borderRadius: 4,
-    margin: 10,
-    marginTop: 0,
-    padding: 4
-  },
-  scrollView: {
-    backgroundColor: '#fff',
-    width: '100%',
-    borderTopColor: '#e6e6e6',
-    borderTopWidth: 1,
-  },
-  linha_conversa: {
-    flexDirection: 'row',
-    paddingLeft: 10,
-    paddingTop: 10,
-    marginRight: 60,
-  }
-})
---
+++ home/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View, Text, StyleSheet
+} from 'react-native';
+
+
+export default function Home() {
+  return (<Text>Home</Text>)
+}
+
+const styles = StyleSheet.create({
+  
+})
